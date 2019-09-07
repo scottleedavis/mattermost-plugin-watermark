adac022681fce286d6a02e25b48251805cee3805
Scott Lee Davis <skawtus@gmail.com>
2019-09-07 14:51:42 -0700

    add jpeg support example

--- decode.go
+++ decode.go
@@ -4,11 +4,14 @@ import (
 	"bufio"
 	"fmt"
 	"image"
+	"image/jpeg"
 	"os"
+	"strings"
 
 	"gopkg.in/auyer/steganography.v2"
 )
 
+//Using the "image/jpeg" module to decode img, err := jpeg.Decode(reader)
 func main() {
 	inFile, err := os.Open(os.Args[1]) // opening file
 	if err != nil {
@@ -16,8 +19,23 @@ func main() {
 	}
 	defer inFile.Close()
 
-	reader := bufio.NewReader(inFile)   // buffer reader
-	img, _, err := image.Decode(reader) // decoding to golang's image.Image
+	var img image.Image
+	if strings.HasSuffix(inFile.Name(), "jpg") {
+		reader := bufio.NewReader(inFile)   // buffer reader
+		img, err = jpeg.Decode(reader) // decoding to golang's image.Image
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	} else {
+		reader := bufio.NewReader(inFile)   // buffer reader
+		img, _, err = image.Decode(reader) // decoding to golang's image.Image
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+	}
+
 	if err != nil {
 		fmt.Println("unable to decode file")
 	}
